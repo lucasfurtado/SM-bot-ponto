2dd999f6c50d52a1e937caa98ec540fae8cd828e
Lucas Furtado <lucasfurtadodev@gmail.com>
2021-09-09 19:54:21 -0300

    bot responding to ping

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
 
 namespace PunchTheClock
 {
@@ -6,7 +8,26 @@ namespace PunchTheClock
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Bot?");
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            DiscordClient discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "ODg1NjM1NzY3MTYwOTQyNjQy.YTp6lQ.RW7OFlZHmr8y8TL_3um-IS7M0HU",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
         }
     }
 }
