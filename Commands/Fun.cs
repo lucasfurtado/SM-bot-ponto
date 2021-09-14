263f2d88e36d565d8b9e97d29a308c222ae5166d
Lucas Furtado <lucasfurtadodev@gmail.com>
2021-09-13 22:08:15 -0300

    daily not working i guess

--- Commands/Fun.cs
+++ Commands/Fun.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using PunchTheClock.Helpers;
+using DSharpPlus.Entities;
+using System.Linq;
 
 namespace PunchTheClock.Commands
 {
     public class Fun : BaseCommandModule
     {
-        [Command("Dorondondon")]
+        [Command("dorondondon")]
         public async Task N4Speed(CommandContext ctx)
         {
             if(ctx.Channel.Id != StaticVariables.ChannelsId.PunchInChannel)
@@ -20,6 +22,29 @@ namespace PunchTheClock.Commands
             else
             {
                 await ctx.RespondAsync(StaticMessages.Unauthorized.UnauthorizedChannel);
+            }
+        }
+
+        [Command("testDaily")]
+        public async Task Pull(CommandContext ctx)
+        {
+            if (ctx.Guild.OwnerId == 437051906599157761)
+            {
+                List<DiscordMember> listDiscordMembers = new List<DiscordMember>();
+                List<ulong> listId = new List<ulong>();
+                var aux = ctx.Guild.Members;
+                for (int i = 0; i < aux.Count; i++)
+                {
+                    listId = aux.Select(x => x.Key).ToList();
+                }
+
+                foreach (var item in listId)
+                {
+                    if(aux.TryGetValue(item, out DiscordMember member))
+                    {
+                        ctx.Channel.PlaceMemberAsync(member);
+                    }
+                }
             }
         }
     }
